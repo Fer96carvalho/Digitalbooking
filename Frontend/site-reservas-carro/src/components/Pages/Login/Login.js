71a86952685bd63fbb6f6d9a5d3ebaa330985e66
Fer96carvalho <Fer96carvalho@root>
2022-02-23 15:59:48 -0400

    Pages, rotas e componentes

---
+++ Frontend/site-reservas-carro/src/components/Pages/Login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Helmet } from 'react-helmet-async';
+
+function Login() {
+
+    return (
+        <>
+        <Helmet>
+            <title>Login</title>
+            </Helmet>
+        </>
+    );
+}
+
+export default Login;
