c219028ac0da6f1145837247b79b53d6de115986
romulloalmeida <romulloalmeida@bol.com>
2022-02-24 06:26:03 -0300

    Criando API de Categorias

---
+++ Backend/src/main/java/com/example/ProjetoIntegradorBackEnd/ProjetoIntegradorBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoIntegradorBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorBackEndApplication.class, args);
+	}
+
+}
