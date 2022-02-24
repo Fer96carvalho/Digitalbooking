97a1d168580833afcef0addb1333683fabd8e82e
marcosspano <e.marcosspano@gmail.com>
2022-02-24 18:33:53 -0300

    Main

---
+++ Frontend/site-reservas-carro/src/pages/Home/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.css';
+
+function Main () {
+    return (
+        <>
+            <main className="container">
+                
+            </main>
+        </>
+    )
+}
+
+export default Main;
