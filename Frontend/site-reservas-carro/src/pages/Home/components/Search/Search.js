d7f75ec64684fd3446f7e72c4d8a98be264fcb91
Fer96carvalho <Fer96carvalho@root>
2022-03-03 10:50:03 -0400

    Finalizando o buscador

--- Frontend/site-reservas-carro/src/pages/Home/components/Search/Search.js
+++ Frontend/site-reservas-carro/src/pages/Home/components/Search/Search.js
@@ -1,32 +1,53 @@
 import React from "react";
-import {Form, Button} from 'react-bootstrap';
+import { Form, Button, Row, Col, Container } from 'react-bootstrap';
 
 function Search() {
     return (
-        <div className="container">
+        <Container fluid>
             <h1>Encontre os melhores carros mais pertos de você</h1>
-
-            <form>
-                <label for="localizacao">Localização</label>
-                <input type="text" id="localizacao" name="localizacao" value=""/>
-                <label for="reserva">Data da reserva</label>
-                <input type="date" id="reserva" name="reserva" value=""/>
-            </form>
-
-            <Form className="w-100 bg-" style={{ maxWidth: "350px" }}>
+            <Form className="w-100 bg-" >
+                <Row>
+                    <Form.Group className="mb-1">
+                        <Form.Label >Cidade</Form.Label>
+                        <Form.Control className="shadow-sm border-0" type="text" placeholder="Sua localização" />
+                    </Form.Group>
+                </Row>
+                <Row>
+                    <Col>
+                    <Form.Group className="mb-1">
+                        <Form.Label>Data da retirada</Form.Label>
+                        <Form.Control className="shadow-sm border-0" type="date" />
+                    </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group className="mb-1">
+                        <Form.Label>Hora da retirada</Form.Label>
+                        <Form.Control className="shadow-sm border-0" type="time" />
+                    </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group className="mb-1">
+                        <Form.Label>Data da devolução</Form.Label>
+                        <Form.Control className="shadow-sm border-0" type="date" />
+                    </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group className="mb-1">
+                        <Form.Label>Hora da devolução</Form.Label>
+                        <Form.Control className="shadow-sm border-0" type="time"  />
+                    </Form.Group>
+                    </Col>
+                </Row>
+               
+                <Form.Group>
+                    <Button className="w-75 text-white fw-bold mt-2" variant="primary" type="submit" >
+                        Buscar
+                    </Button>
+                </Form.Group>
                 
-                <Form.Group className="mb-1">
-                    <Form.Label >Cidade</Form.Label>
-                    <Form.Control className="shadow-sm border-0" type="text" placeholder="Sua localização" style={{ maxWidth: "250px" }}/>
-                    <Form.Label>Periodo de reserva</Form.Label>
-                    <Form.Control className="shadow-sm border-0" type="date" style={{ maxWidth: "180px" }}/>
-                    <Button className="w-75 text-white fw-bold mt-4" variant="primary" type="submit" style={{ maxWidth: "180px" }}>
-                    Buscar
-                </Button>
-                </Form.Group>  
-                                    
+
             </Form>
-        </div>
+        </Container >
     )
 }
 
