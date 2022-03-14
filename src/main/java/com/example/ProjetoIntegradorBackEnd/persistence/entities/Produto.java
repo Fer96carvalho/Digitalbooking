2e10948b49d77893c8431af5700b9ee553fe1fbb
marcosspano <e.marcosspano@gmail.com>
2022-03-14 19:15:20 -0300

    Adicionar relacionamento 'Muitos para um' entre cidade e produto

--- src/main/java/com/example/ProjetoIntegradorBackEnd/persistence/entities/Produto.java
+++ src/main/java/com/example/ProjetoIntegradorBackEnd/persistence/entities/Produto.java
@@ -1,12 +1,10 @@
 package com.example.ProjetoIntegradorBackEnd.persistence.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
 
 @Entity
 @Setter@Getter
@@ -16,6 +14,13 @@ public class Produto {
     @SequenceGenerator(name = "produto_sequencia", sequenceName = "produto_sequencia", allocationSize = 1)
     private Integer id;
     private String nome;
+
     @Lob
     private String descricao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cidade_id", foreignKey = @ForeignKey(name = "fk_cidade"))
+    @JsonIgnore
+    private Cidade cidade;
+    
 }
