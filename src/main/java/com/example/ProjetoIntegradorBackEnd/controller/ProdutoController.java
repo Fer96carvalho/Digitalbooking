2ed3053b634696f0c92bf5b9ccd88f1746c37e99
romulloalmeida <romulloalmeida@bol.com>
2022-03-17 10:09:09 -0300

    Criacao da controller de Produto.

---
+++ src/main/java/com/example/ProjetoIntegradorBackEnd/controller/ProdutoController.java
@@ -0,0 +1,41 @@
+package com.example.ProjetoIntegradorBackEnd.controller;
+
+import com.example.ProjetoIntegradorBackEnd.persistence.entities.Produto;
+import com.example.ProjetoIntegradorBackEnd.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService service;
+
+    @GetMapping
+    public List<Produto> getProduto(){
+        return service.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public Produto getProdutoById(@PathVariable Integer id){
+        return service.findById(id);
+    }
+
+    @GetMapping("/cidade/{cidade}")
+    public List<Produto> getProdutoByCidade(@PathVariable String cidade){
+        return service.findByCidade(cidade);
+    }
+
+    @GetMapping("/categoria/{categoria}")
+    public List<Produto> getProdutoByCategoria(@PathVariable String categoria){
+        return service.findByCategoria(categoria);
+    }
+
+    @PutMapping
+    public Produto putProduto(@RequestBody Produto produto){
+        return service.createProduto(produto);
+    }
+}
