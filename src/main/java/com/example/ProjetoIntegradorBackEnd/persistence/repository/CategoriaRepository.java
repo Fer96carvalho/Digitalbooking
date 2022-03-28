a8edfa6cd60b042b9fd7e7cd0d9c230fa618c533
romulloalmeida <romulloalmeida@bol.com>
2022-03-28 08:26:18 -0300

    Remoção dos elementos de Backend na partição de Fronte nd

--- src/main/java/com/example/ProjetoIntegradorBackEnd/persistence/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProjetoIntegradorBackEnd.persistence.repository;
-
-import com.example.ProjetoIntegradorBackEnd.persistence.entities.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
-}
--- src/main/java/com/example/ProjetoIntegradorBackEnd/persistence/repository/CidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProjetoIntegradorBackEnd.persistence.repository;
-
-import com.example.ProjetoIntegradorBackEnd.persistence.entities.Cidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CidadeRepository extends JpaRepository<Cidade,Integer> {
-}
