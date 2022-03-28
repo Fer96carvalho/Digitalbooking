524ba4b33fe92ad2a4e1f83295fa545db7085ea7
romulloalmeida <romulloalmeida@bol.com>
2022-03-28 14:59:10 -0300

    Configuração de cors.

--- src/main/java/com/example/ProjetoIntegradorBackEnd/configuracao/CorsConfig.java
+++ src/main/java/com/example/ProjetoIntegradorBackEnd/configuracao/CorsConfig.java
@@ -10,18 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-<<<<<<< HEAD:src/main/java/com/example/ProjetoIntegradorBackEnd/configuracao/CorsConfiguration.java
-        //liberando app cliente 1
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowCredentials(true);
-=======
-        //liberando cliente
-        registry.addMapping("/*")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-
->>>>>>> 16a14c4026c729898ffeac95dfd079da741704a9:src/main/java/com/example/ProjetoIntegradorBackEnd/configuracao/CorsConfig.java
     }
 }
