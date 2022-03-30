620f89f80a40300efbbf662c6f4d031d6bbd0596
marcosspano <e.marcosspano@gmail.com>
2022-03-30 17:57:52 -0300

    alteração das classes usuario, funcao, imagem e produto

--- src/main/java/com/example/ProjetoIntegradorBackEnd/persistence/entities/Funcao.java
+++ src/main/java/com/example/ProjetoIntegradorBackEnd/persistence/entities/Funcao.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Getter@Setter
@@ -13,5 +14,7 @@ public class Funcao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false,nullable = false)
     private Integer id;
+
+    @NotBlank(message = "Nome não deve estar vazio")
     private String nome;
 }
