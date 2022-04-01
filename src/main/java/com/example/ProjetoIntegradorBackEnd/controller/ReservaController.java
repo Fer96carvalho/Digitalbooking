ecc5116448f514f5c61da394f845a9fbca126fc0
romulloalmeida <romulloalmeida@bol.com>
2022-04-01 07:50:39 -0300

    Criacao da ReservaController.

---
+++ src/main/java/com/example/ProjetoIntegradorBackEnd/controller/ReservaController.java
@@ -0,0 +1,26 @@
+package com.example.ProjetoIntegradorBackEnd.controller;
+
+import com.example.ProjetoIntegradorBackEnd.persistence.entities.Reserva;
+import com.example.ProjetoIntegradorBackEnd.service.ReservaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("reserva")
+public class ReservaController {
+
+    @Autowired
+    private ReservaService service;
+
+    @GetMapping("/{id}")
+    public List<Reserva> getReservaByProduto(@PathVariable Integer id){
+        return service.findByProdutoId(id);
+    }
+
+    @PostMapping
+    public Reserva postReserva(@RequestBody Reserva reserva){
+        return service.createReserva(reserva);
+    }
+}
