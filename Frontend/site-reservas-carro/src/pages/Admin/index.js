6fff72635be0c1f8858288a29dec2a8bafcdf81e
Fer96carvalho <Fer96carvalho@root>
2022-04-28 10:06:46 -0400

    Criaçao de prosuto em andamento

--- Frontend/site-reservas-carro/src/pages/Admin/index.js
+++ Frontend/site-reservas-carro/src/pages/Admin/index.js
@@ -33,11 +33,11 @@ export function Admin() {
     resolver: yupResolver(schema),
   });
 
- async function handleSubmitForm(data) {
-  const dataUser = JSON.parse(localStorage.getItem("@SESSION"));
-  console.log("DATA", data);
-  console.log("IMAGES", images);
-  console.log("attributes", attributes);
+  async function handleSubmitForm(data) {
+    const dataUser = JSON.parse(localStorage.getItem("@SESSION"));
+    console.log("DATA", data);
+    console.log("IMAGES", images);
+    console.log("attributes", attributes);
     const resgistrarReserva = await api.post('/produto', {
       nome: data.name_vehicle,
       descricao: data.description,
@@ -51,82 +51,102 @@ export function Admin() {
       regras: data.car_rules,
       cancelar: data.cancel,
       seguranca: data.security,
-      imagem: "Imagens",
-      endereco: data.address      
+      imagem: '',
+      endereco: data.address
     }, {
-      headers:{
-        'Authorization': `Bearer ${dataUser.token}` 
+      headers: {
+        'Authorization': `Bearer ${dataUser.token}`
       }
-    }).then((response)=>{
-      if (response.status === 200){
-        console.log("Criação de Produto Efetuada com sucesso!")
+    }).then((response) => {
+      if (response.status === 200) {
+        api.get(`/produto/nome/${data.name_vehicle}`, {
+          header: {
+            'Authorization': `Bearer ${dataUser.token}`
+          }
+        }).then((response) => {
+          if (response.status === 200) {
+            let id = response.data.id;
+            for (let i = 0; i <= images.length; i++) {
+              api.post('/imagens', {
+                url: images[i].url,
+                produto: {
+                  id: id
+                }
+              }, {
+                header: {
+                  'Authorization': `Bearer ${dataUser.token}`
+                }
+              })
+            }}
+            return console.log("Falha ao buscar o id do produto")
+          })
+
       } else {
-        console.log("Erro não corrigivel a tempo!")
+        return console.log("Erro ao criar o produto!")
       }
-    })
+    });
+    return console.log("Criação de Produto Efetuada com sucesso!")
+} 
+  
 
+const [dataCidades, setDataCidades] = useState([]);
+const [dataCategorias, setDataCategorias] = useState([]);
 
-    
-  }
-
-  const [dataCidades, setDataCidades] = useState([]);
-  const [dataCategorias, setDataCategorias] = useState([]);
- 
 
 const getCidades = async () => {
   await api.get('/cidade')
-  .then(response => {
-    const data1 = response.data.map(({id, nome})=>{
-      return {
-        value: id, label: nome
-      }
+    .then(response => {
+      const data1 = response.data.map(({ id, nome }) => {
+        return {
+          value: id, label: nome
+        }
+      })
+      setDataCidades(data1)
     })
-    setDataCidades(data1)
-  })
-  .catch((err) => console.error(err));
+    .catch((err) => console.error(err));
 }
 
 
-const getCategorias =  () => {
+const getCategorias = () => {
   api.get('/categoria')
-  .then(response => {
-    const data = response.data.map(({id, titulo})=>{
-      return {
-        value: id, label: titulo
-      }
-    })
-    setDataCategorias(data)
+    .then(response => {
+      const data = response.data.map(({ id, titulo }) => {
+        return {
+          value: id, label: titulo
+        }
+      })
+      setDataCategorias(data)
 
-  })
-  .catch((err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
 }
 
-useEffect(()=>{
+useEffect(() => {
   getCategorias()
   getCidades()
-},[])
+}, [])
 
 
 
 
-  return (
-    <>
-      <Container as="section" fluid className={`${styled.red} m-0 mb-5`}>
-        <Container fluid className={`pb-2 pt-4 px-0 m-0 mx-auto max-width-1180`}>
-          <h2 className="fs-4 font-600">Criar Veículo</h2>
-        </Container>
-        <Container fluid className={`py-3 m-0 mx-auto max-width-1180 rounded ${styled.container}`}>
-          <form onSubmit={handleSubmit(handleSubmitForm)}>
-            <CarInfo Controller={Controller} control={control} register={register} errors={errors} category={dataCategorias} city={dataCidades}/>
-            <Attibutes setAttributes={setAttributes} attributes={attributes} />
-            <ProductPolicies register={register} errors={errors} />
-            <Images images={images} setImages={setImages} />
-            <div className='d-flex justify-content-center align-items-center mt-5 mb-3'>
-              <Button type='submit' className={`w-100 ${styled.max_width} text-white font-600`}>Criar</Button>
-            </div>
-          </form>
-        </Container>
+return (
+  <>
+    <Container as="section" fluid className={`${styled.red} m-0 mb-5`}>
+      <Container fluid className={`pb-2 pt-4 px-0 m-0 mx-auto max-width-1180`}>
+        <h2 className="fs-4 font-600">Criar Veículo</h2>
       </Container>
-    </>
-  )
+      <Container fluid className={`py-3 m-0 mx-auto max-width-1180 rounded ${styled.container}`}>
+        <form onSubmit={handleSubmit(handleSubmitForm)}>
+          <CarInfo Controller={Controller} control={control} register={register} errors={errors} category={dataCategorias} city={dataCidades} />
+          <Attibutes setAttributes={setAttributes} attributes={attributes} />
+          <ProductPolicies register={register} errors={errors} />
+          <Images images={images} setImages={setImages} />
+          <div className='d-flex justify-content-center align-items-center mt-5 mb-3'>
+            <Button type='submit' className={`w-100 ${styled.max_width} text-white font-600`}>Criar</Button>
+          </div>
+        </form>
+      </Container>
+    </Container>
+  </>
+)
 }
